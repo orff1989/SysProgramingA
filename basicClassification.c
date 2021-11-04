53d96b41ead2bce6b4e9c599d917aee21f460970
yuvili <77205478+yuvili@users.noreply.github.com>
2021-11-04 14:18:19 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
-#include <math.h>
+#include<math.h>
 
 int isPrime(int num){
-    if (num<=1) return 0;
-    
-double sq = sqrt(num);
-for (int i = 2; i <= sq; i++){
-    if (num%i==0) return 0;
+    if(num == 1 || num == 2){
+        return 1;
     }
-
+    for (int i = 2; i < sqrt(num); i++)
+    {
+        if(num % i == 0){
+            return 0;
+        }
+    }
     return 1;
+    
 }
 
-int factorial(int n){
-    if(n==1 || n==0) return 1;
-    int sum=1;
-    while (n>1)
-    {
-        sum=sum*n;
-        n--;
+int factorial(int num){
+    if(num == 0 || num == 1){
+        return 1;
     }
-    return sum;
+    else{
+        return num * factorial(num - 1);
+    }
 }
 
 int isStrong(int num){
-int n=num;
-int digit;
-int sum=0;
-if(num==0) return 1;
-while (n!=0){
-    digit=n%10;
-    sum+=factorial(digit);
-    n=n/10;
+    int n = num;
+    int digit;
+    int sum = 0;
+    
+    if(num == 0){
+        return 1;
+    } 
+    
+    while (n!=0){
+        digit = n%10;
+        sum += factorial(digit);
+        n = n/10;
+    }
+    
+    if(sum == num){
+        return 1;
+    } 
+    return 0;
 }
- if(sum==num) return 1;
- else return 0;
-} 
 
